7e0363abf61915347a362d162d4e91b297592b98
Rodrigo Jantsch <rodrigo_jantsch@hotmail.com>
2017-04-06 11:57:11 -0700

    Final Project

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 var express = require('express')
 var app = express()
+var port = process.env.PORT || 3000;
+var uaParser = require('ua-parser');
+var parseAcceptLanguage = require('parse-accept-language');
+
 
 app.get('/', function (req, res) {
+    
+    var ip = req.ip;
+    //get ip address with express build-in method req.ip
+    
+    var language = parseAcceptLanguage(req);
+    //get an array of preferred languages from http req headers, and take the first one, which is most preferred language
+        
+    var uaHeader = req.headers['user-agent'];
+    
+    var agent = uaParser.parseOS(uaHeader).toString();
+
+     
+     res.json({"ipaddress": ip, "language": language[0], "software": agent});
 
 })
 
-app.listen(process.env.PORT || 5000, function () {
-  console.log('Example app listening on port '+process.env.PORT || 5000+'!')
+
+
+app.listen(port, function () {
+  console.log('Example app listening on port '+port+'!')
 })
