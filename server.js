3f816e849e89aa500e46a50251dfcd1348ced481
Rodrigo Jantsch <rodrigo_jantsch@hotmail.com>
2017-04-06 12:08:59 -0700

    Final Project

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var parseAcceptLanguage = require('parse-accept-language');
 app.get('/', function (req, res) {
     
     var ip = req.ip;
-    //get ip address with express build-in method req.ip
+     if (ip.substr(0, 7) == '::ffff:') {
+      ip = ip.substr(7);
+    }
     
     var language = parseAcceptLanguage(req);
     //get an array of preferred languages from http req headers, and take the first one, which is most preferred language
